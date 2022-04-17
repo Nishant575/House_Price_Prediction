5fd27906d248b14ff2c5a2aa654d16249b75900c
Nishant Dalvi <61659609+Nishant575@users.noreply.github.com>
2022-04-17 14:27:31 +0530

    second commit

--- hp/util.py
+++ hp/util.py
@@ -28,13 +28,13 @@ def load_saved_artifacts():
     global  __data_columns
     global __locations
 
-    with open("/Users/apple/Nishant/Projects/SEMVI_Proj/House_Prediction/hp/model/columns.json", "r") as f:
+    with open("/Users/apple/Nishant/Projects/SEMVI_Proj/House_Price_Prediction/hp/model/columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:] 
 
     global __model
     if __model is None:
-        with open('/Users/apple/Nishant/Projects/SEMVI_Proj/House_Prediction/hp/model/hp_model.pickle', 'rb') as f:
+        with open('/Users/apple/Nishant/Projects/SEMVI_Proj/House_Price_Prediction/hp/model/hp_model.pickle', 'rb') as f:
             __model = pickle.load(f)
 
 load_saved_artifacts()
