dba9ba1a3d210797440f4625b4937fe275519de5
Nishant575 <n.dalvi2010@gmail.com>
2022-06-18 09:47:59 +0530

    commit

--- hp/util.py
+++ hp/util.py
@@ -41,7 +41,7 @@ def buy_price(location):
         price = abs(round(__model.predict([x])[0],2))
         graph_dict[sqft[i]] = price
     json_object = json.dumps(graph_dict, indent = 4)
-    with open("F:\\Nishant\\SemVIProj\\House_Price_Prediction\\hp\\model\\graph.json", "w") as outfile:
+    with open("artifacts/graph.json", "w") as outfile:
         outfile.write(json_object)
 
 def get_location_names():
@@ -51,7 +51,7 @@ def load_saved_artifacts():
     global  __data_columns
     global __locations
 
-    with open('F:\\Nishant\\SemVIProj\\House_Price_Prediction\\hp\\model\\columns.json', "r") as f:
+    with open("artifacts/columns.json", "r") as f:
 
 
         __data_columns = json.load(f)['data_columns']
@@ -60,7 +60,7 @@ def load_saved_artifacts():
     global __model
     if __model is None:
 
-        with open('F:\\Nishant\\SemVIProj\\House_Price_Prediction\\hp\\model\\hp_model.pickle', 'rb') as f:
+        with open("artifacts/hp_model.pickle", 'rb') as f:
 
             __model = pickle.load(f)
 
